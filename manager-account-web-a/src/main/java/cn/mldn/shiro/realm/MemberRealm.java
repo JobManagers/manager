64317ed2ce34892c90b29bc9efdcc4d605ba8d82
'mldnwu' <'s2288156@163.com'>
2017-06-13 16:19:42 +0800

    CA登录成功，redis服务器进行session和权限保存

--- manager-account-web-a/src/main/java/cn/mldn/shiro/realm/MemberRealm.java
+++ manager-account-web-a/src/main/java/cn/mldn/shiro/realm/MemberRealm.java
@@ -1,72 +1,73 @@
 package cn.mldn.shiro.realm;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.cas.CasRealm;
+import org.apache.shiro.cas.CasToken;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.util.CollectionUtils;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.validation.Assertion;
+import org.jasig.cas.client.validation.TicketValidator;
 
 import cn.mldn.api.IMemberService;
-import cn.mldn.dto.MemberDto;
-import cn.mldn.util.enctype.PasswordUtil;
 
-public class MemberRealm extends AuthorizingRealm {
+public class MemberRealm extends CasRealm {
 	@Resource
 	private IMemberService memberService;
-
 	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
+			throws AuthenticationException {
+		
+		System.out.println("=================================== 1、进行认证操作处理 ======================================");
+		// CAS认证之后需要返回用户名和票根数据，那么现在需要进行两方面的处理，一方面是进行票根检测，另外一方面是进行用户名的数据返回
+		CasToken casToken = (CasToken) token ; 	// 接收CAS返回的Token数据
+		if(casToken == null){	// 没有返回CAS的认证的Token信息，则表示认证出现问题
+			return null ;	// 此处没有认证信息返回
+		}
+		String ticket = (String) casToken.getCredentials() ;	// 取得认证的票根数据
+		// 如果此时返回的CAS票根不正确，那么将出现错误
+		if(!org.apache.shiro.util.StringUtils.hasText(ticket)){	// 现在需要进行票根数据的检测处理
+			return null ; 	// 此时没有认证信息返回
+		}
+		TicketValidator ticketValidator = super.ensureTicketValidator() ;	// 需要确保票根是正确的
+		try{
+			// 现在可以确一个CAS的检测结果
+			Assertion casAssertion = ticketValidator.validate(ticket, super.getCasService()) ;	// 进行票根验证处理
+			// 通过cas还可以获取用户名
+			AttributePrincipal casPrincipal = casAssertion.getPrincipal() ;	// 获取用户信息
+			String mid = casPrincipal.getName() ;	// 获取用户名
+			// 需要设置一个集合，以返回所有的所需要的信息
+			List principals = CollectionUtils.asList(mid,casPrincipal.getAttributes()) ;	// 获得用户信息
+			PrincipalCollection collection = new SimplePrincipalCollection(principals, super.getName()) ;
+			return new SimpleAuthenticationInfo(collection, ticket);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return super.doGetAuthenticationInfo(token);
+	}
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(
+			PrincipalCollection principals) {
 		System.out.println("++++++++++++++ 2、进行授权操作处理 ++++++++++++++");
 		// 该操作的主要目的是取得授权信息，说的直白一点就是角色和权限数据
 		SimpleAuthorizationInfo auth = new SimpleAuthorizationInfo();
 		// 执行到此方法的时候一定是已经进行过用户认证处理了（用户名和密码一定是正确的）
 		String mid = (String) principals.getPrimaryPrincipal(); // 取得用户名
-		try {
-			Map<String, Set<String>> map = memberService.getRoleAndAction(mid);
-			auth.setRoles(map.get("allRoles")); // 保存所有的角色
-			auth.setStringPermissions(map.get("allActions")); // 保存所有的权限
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return auth;
-	}
-
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
-			throws AuthenticationException {
-		System.out.println("============== 1、进行认证操作处理 ==============");
-		String userid = token.getPrincipal().toString(); // 用户名
-		MemberDto member = memberService.get(userid); // 通过用户名获取用户信息
-		System.out.println("2" + member);
-		if (member == null) { // 表示该用户信息不存在，不存在则应该抛出一个异常
-			throw new UnknownAccountException("搞什么搞，用户名不存在！");
-		}
-		// 用户名如果存在了，那么就需要确定密码是否正确
-		String password = PasswordUtil.getPassword(new String((char []) token.getCredentials()));
-		if (!password.equals(member.getPassword())) { // 密码验证
-			throw new IncorrectCredentialsException("密码都记不住，去死吧！");
-		}
-		// 随后还需要考虑用户被锁定的问题
-		if (member.getLocked().equals(1)) { // 1表示非0，非0就是true
-			throw new LockedAccountException("被锁了，求解锁去吧！");
-		}
-		// 定义需要进行返回的操作数据信息项
-		SimpleAuthenticationInfo auth = new SimpleAuthenticationInfo(token.getPrincipal(),
-				password, "memberRealm");
-		SecurityUtils.getSubject().getSession().setAttribute("name", "我的名字");
-		System.out.println("2" + member);
+		Map<String, Set<String>> map = this.memberService.getRoleAndAction(mid);
+		auth.setRoles(map.get("allRoles")); // 保存所有的角色
+		auth.setStringPermissions(map.get("allActions")); // 保存所有的权限
 		return auth;
 	}
 }
